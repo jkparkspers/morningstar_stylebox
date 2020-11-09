9691b662917aee0f80b935f4f870f9c05eebdd94
jkparkspers <jkparksbus@outlook.com>
2020-11-09 18:10:15 -0500

    add test pkgs, test holdings

---
+++ test/unit/csv_holdings.py
@@ -0,0 +1,10 @@
+titles = ['ticker','name','class','sv','sb','sg','mv','mb','mg','lv','lb','lg','Unknown' ,'Unavailable' ]
+h0 = ['DFEOX','DFA US CORE EQ 1 I','US',"$5,735 ","$6,457 ","$4,077 ","$10,040 ","$10,305 ","$9,115 ","$30,106 ","$28,384 ","$22,383 ",'$0' ,'$0' ]
+h1 = ['DFEVX','DFA EMERG MKTS VAL I','EM',"$3,592 ",'$903' ,'$178' ,"$6,111 ","$1,747 ",'$371' ,"$16,834 ","$6,581 ",'$84' ,'$0' ,'$0' ]
+h2 = ['DFFVX','DFA US TARGET VAL I','US',"$28,858 ","$23,705 ","$8,588 ","$12,946 ","$5,825 ",'$883' ,'$219' ,'$0' ,'$0' ,'$0' ,'$0' ]
+
+
+hold0 = dict(zip(titles, h0))
+hold1 = dict(zip(titles, h1))
+hold2 = dict(zip(titles, h2))
+
