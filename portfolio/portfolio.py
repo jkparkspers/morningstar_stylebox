f14c45dab6e5aa501761624e9329ede1eb6413b3
jkparkspers <jkparksbus@outlook.com>
2020-11-09 17:27:08 -0500

    stub for portfolio class

---
+++ portfolio/portfolio.py
@@ -0,0 +1,27 @@
+"""
+A Portfolio is a collection of holdings.
+"""
+
+
+class Portfolio(dict):
+
+    def __init__(self):
+        super().__init__()
+
+    """
+    Create a new sub-portfolio based on a given predicate.  The existing
+    portfolio is unchanged.
+    """
+
+    def sub_port(self, predicate):
+        new_port = Portfolio()
+        for holding in self:
+            if predicate(holding):
+                new_port.append(holding)
+        return new_port
+
+    def total(self, predicate=lambda p: True):
+        tot = 0
+        for h in self:
+            tot += h.total(predicate)
+        return tot
