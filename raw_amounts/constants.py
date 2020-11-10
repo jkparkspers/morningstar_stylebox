f2423a8974eeac8c1453973bdf7ab4751f426228
jkparkspers <jkparksbus@outlook.com>
2020-11-10 14:51:21 -0500

    add Holding pytest

--- raw_amounts/constants.py
+++ raw_amounts/constants.py
@@ -1,1 +1,2 @@
 box_titles = ['sv', 'sb', 'sg', 'mv', 'mb', 'mg', 'lv', 'lb', 'lg']
+all_titles = ['ticker', 'name', 'class', 'sv', 'sb', 'sg', 'mv', 'mb', 'mg', 'lv', 'lb', 'lg', 'Unknown', 'Unavailable']
---
+++ test/unit/test_holding.py
@@ -0,0 +1,21 @@
+import pytest
+from raw_amounts.holding import Holding
+from test.unit.csv_holdings import hold0, hold1, hold2
+
+
+def test_total0():
+    holding = Holding(hold0)
+
+    assert 9078 == holding.total()
+
+def test_total1():
+    holding = Holding(hold1)
+    assert 2007 == holding.total(lambda k : k == 'mb')
+
+def test_total2():
+    holding = Holding(hold2)
+    assert 9049 == holding.total(lambda k : k[0] == 'l')
+
+def test_total3():
+    holding = Holding(hold0)
+    assert 3033 == holding.total(lambda k : k[1] == 'g')
